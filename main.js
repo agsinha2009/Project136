bcc8fd7d26edc1827005496b73f2e55c92851254
agsinha2009 <101357307+agsinha2009@users.noreply.github.com>
2023-08-20 11:01:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+status="";
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.position(650,300);
+    video=createCapture(VIDEO);
+    video.size(480,380);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
+
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting object";
+}
+
+function modelLoaded(){
+    console.log("Model is loaded");
+    status=true;
+}
